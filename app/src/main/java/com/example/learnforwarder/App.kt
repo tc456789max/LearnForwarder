5dc44d0667cb4f29465be85d80cccf6a99ec9d25
tc456789max <tc456789max@users.noreply.github.com>
2025-12-30 07:30:11 -0500

    Fix BuildConfig generation and import

--- app/src/main/java/com/example/learnforwarder/App.kt
+++ app/src/main/java/com/example/learnforwarder/App.kt
@@ -2,6 +2,7 @@ package com.example.learnforwarder
 
 import android.app.Application
 import timber.log.Timber
+import com.example.learnforwarder.BuildConfig
 
 class App : Application() {
     override fun onCreate() {
